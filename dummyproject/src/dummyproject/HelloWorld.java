2ba40d5010f154c538f56b4719cfe24cf2fee0c6
himanshu <himanshu@amd-PC>
2018-09-01 12:19:07 +0530

    Initial Commit.

---
+++ dummyproject/src/dummyproject/HelloWorld.java
@@ -0,0 +1,13 @@
+package dummyproject;
+import hibiscus.flower;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World \n\n");
+		flower.dummyflowermessage();
+
+	}
+
+}
---
+++ dummyproject/src/hibiscus/flower.java
@@ -0,0 +1,10 @@
+package hibiscus;
+
+public class flower {
+
+	public static void dummyflowermessage()
+	{
+		
+		System.out.println("Flower Class");
+	}
+}
