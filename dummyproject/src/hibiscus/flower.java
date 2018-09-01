a423a27063a65e09ed405e695bb1611bbac0c0d6
ubuntu <a@a>
2018-09-01 16:38:04 +0530

    test

--- dummyproject/src/hibiscus/flower.java
+++ dummyproject/src/hibiscus/flower.java
@@ -4,8 +4,9 @@ public class flower {
 
 	public static void dummyflowermessage()
 	{
-		
+		//comment by user 1
 		System.out.println("Flower Class");
 		System.out.println("New line 2");
+		
 	}
 }
